63ece4146dfa11d94595ee57341871b7245c68d5
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2020-05-24 13:51:40 -0500

    fix climate

--- frontend/data.py
+++ frontend/data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import geopandas
+import streamlit as st
 
 data_folder = '../data'
 
@@ -28,3 +30,79 @@ temperature = t_year.temperature.transpose()
 p_year = accidents_All[['year', 'precipIntensity']].groupby('year').agg(agg)
 
 precipitation = p_year.precipIntensity.transpose()
+
+
+# bourought
+
+borough_file = os.path.join(data_folder, 'poligonos-localidades.geojson')
+borough_df = geopandas.read_file(borough_file, driver= "GeoJSON")
+borough_df.drop(columns = ['Acto administrativo de la localidad', 'Area de la localidad'], inplace=True)
+borough_df.rename(columns={'Nombre de la localidad':'borough', 'Area de la localidad':'area',
+                           'Identificador unico de la localidad':'id' }, inplace=True)
+borough_df.id = borough_df.id.astype(int)
+
+borough_info = os.path.join(data_folder, 'localidades.csv')
+borough_info = pd.read_csv(borough_info, header=0, dtype='str')
+borough_info.area_km2 = borough_info.area_km2.astype('float')
+borough_info.population = borough_info.population.astype('int')
+borough_info.population_density = borough_info.population_density.astype('float')
+borough_info.id = borough_info.id.astype(int)
+
+borough_df = borough_df.merge(borough_info, how='inner', left_on='id', right_on='id')
+borough_df.drop(columns = ['borough_y'], inplace=True)
+borough_df.rename(columns={'borough_x':'borough'}, inplace=True)
+
+# set(accidents.borough.unique()) - set(borough_df['borough'].unique())
+# st.write(accidents)
+
+by_borough = accidents[['borough_geo','accident_id']].groupby(['borough_geo']).count().reset_index().sort_values(by='accident_id',ascending=False)
+by_borough = by_borough.reset_index(drop=True)
+by_borough.rename(columns={'accident_id':'accident_count'}, inplace=True)
+#
+borough_data = borough_df.merge(by_borough, how='left', left_on='borough', right_on='borough_geo')
+#
+borough_data['accident_density'] = round(borough_data.accident_count/borough_data.area_km2, 1)
+borough_data['accident_density_population'] = borough_data.accident_count/borough_data.population
+
+
+aa = borough_data
+
+with open('data1.json', 'w') as f:
+  f.write(borough_data.to_json())
+
+# Custom color scale
+COLOR_RANGE = [
+    [65, 182, 196],
+    [127, 205, 187],
+    [199, 233, 180],
+    [237, 248, 177],
+    [255, 255, 204],
+    [255, 237, 160],
+    [254, 217, 118],
+    [254, 178, 76],
+    [253, 141, 60],
+    [252, 78, 42],
+    [227, 26, 28],
+    [189, 0, 38],
+    [128, 0, 38],
+]
+
+BREAKS = [-0.6, -0.45, -0.3, -0.15, 0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05, 1.2]
+
+
+def color_scale(val):
+    for i, b in enumerate(BREAKS):
+        if val < b:
+            return COLOR_RANGE[i]
+    return COLOR_RANGE[i]
+
+
+json = pd.read_json('data1.json')
+df = pd.DataFrame()
+df["coordinates"] = json["features"].apply(lambda row: row["geometry"]["coordinates"])
+df["accident_density"] = json["features"].apply(lambda row: row["properties"]["accident_density"])
+df["accident_count"] = json["features"].apply(lambda row: row["properties"]["accident_count"])
+df["fill_color"] = json["features"].apply(lambda row: color_scale(row["properties"]["accident_count"]))
+
+
+borough_data = df
--- frontend/pages/climate.py
+++ frontend/pages/climate.py
@@ -1,4 +1,41 @@
 import streamlit as st
+import pydeck as pdk
+import pandas as pd
+from data import borough_data,aa
+# from data import accidents
 
 def write():
   st.title('Climate')
+
+  # data = borough_data.head(1).to_json()
+  # data = '../data/borough.json'
+  # st.write(borough_data.head(1)['geometry'].data)
+  
+  type_g = st.radio('classify by', ['accident_density_population', 'accident_density', 'accident_count'])
+  st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=4.480335,
+        longitude=-74.083644,
+        zoom=10,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+          "GeoJsonLayer",
+          "http://192.168.1.11:8080/data1.json",
+          id="geojson",
+          opacity=0.8,
+          stroked=False,
+          filled=True,
+          extruded=True,
+          wireframe=True,
+          get_elevation=f"properties.{type_g}",
+          get_fill_color="[48, 128, properties.accident_density * 255, 255]",
+          get_line_color=[255, 255, 255],
+        )
+    ],
+    
+  ))
+
+  
